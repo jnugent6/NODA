7d3dfcaaec1b5e8a75f24ace15b2c705d04892bd
jnugent6 <jnugent6@gatech.edu>
2015-09-26 09:08:00 -0400

    James update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,27 +2,6 @@ class PagesController < ApplicationController
 	def home
 	end
 
-	def new
-		@numPlayers = Game.new
-	end
-
-    def create 
-      @numPlayers = Game.new(message_params) 
-      if @message.save 
-        redirect_to 'info' 
-      else 
-        render 'new' 
-      end 
-    end
-
-    private 
-    def message_params 
-        params.require(:game).permit(:players) 
-    end
-
-	def setup
-	end
-
     def start
     end
 
--- db/migrate/20150926070054_create_games.rb
+++
@@ -1,8 +0,0 @@
-class CreateGames < ActiveRecord::Migration
-  def change
-    create_table :games do |t|
-    	t.integer :players
-      t.timestamps
-    end
-  end
-end
